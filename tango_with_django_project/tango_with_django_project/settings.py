2f7bff3720061c4677f72b5fb22963d2783b2267
JosefHorn <46573109+JosefHorn@users.noreply.github.com>
2019-02-07 16:51:03 +0000

    up to 9.7

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -49,6 +49,11 @@ INSTALLED_APPS = [
     'rango',
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -98,6 +103,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'OPTIONS':{'min_length':6,},
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
